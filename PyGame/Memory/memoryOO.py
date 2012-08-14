6abfb06779a045a2bbde5fafa3e290d2ef50223f
develru <develru@me.com>
2012-08-14 10:46:10 +0200

    update from C

--- PyGame/Memory/memoryOO.py
+++ PyGame/Memory/memoryOO.py
@@ -19,8 +19,7 @@ class Card(object):
         self.__coords = coords
         self.__shape = shape
         self.__color = color
-        #TODO: change to False
-        self.covered = True
+        self.covered = False
 
     def draw(self, displaySurf):
         """Dra the card"""
@@ -55,12 +54,30 @@ class Card(object):
             pygame.draw.rect(displaySurf, Game.boxColor, (left, top,
                 Game.boxSize, Game.boxSize))
 
+    def coverAnimation(self, displaySurf):
+        """Animate the cover"""
+        for coverage in range(0, Game.boxSize + Game.animSpeed,
+                Game.animSpeed):
+            pygame.draw.rect(displaySurf, Game.boxColor, (self.__coords[0],
+                self.__coords[1], coverage, Game.boxSize))
+            pygame.display.flip()
+            pygame.time.wait(50)
+
+    def getCoords(self):
+        """Return with the coords of the card"""
+        return self.__coords
+
+    def getShapeAndColor(self):
+        """Return with the shape and the color of the card"""
+        return (self.__shape, self.__color)
+
 
 class Game(object):
     windowWidth = 640
     windowHeight = 480
     boxSize = 40
     gapSize = 10
+    animSpeed = 8
     boardWith = 10
     boardHeight = 7
     assert (boardWith * boardHeight) % 2 == 0, \
@@ -87,6 +104,7 @@ class Game(object):
 
     bgColor = navyBlue
     boxColor = white
+    highlightColor = blue
     allColors = (red, green, blue, yellow,
             orange, purple, cyan)
     allShapes = (donaut, square, diamond, lines,
@@ -99,6 +117,7 @@ class Game(object):
         """docstring for __init__"""
 
         self.__cards = self.getRandomizedCards()
+        self.__firstCard = None
 
         self.displaySurf = pygame.display.set_mode((Game.windowWidth,
             Game.windowHeight))
@@ -136,21 +155,67 @@ class Game(object):
 
     def update(self):
         """Update the game graphic"""
+        self.displaySurf.fill(Game.bgColor)
         for card in self.__cards:
             card.draw(self.displaySurf)
+
+    def getCardAtPixel(self, x, y):
+        """Return with the card under the cursor"""
+        for boxX in range(Game.boardWith):
+            for boxY in range(Game.boardHeight):
+                left, top = self.leftTopCoordsOfCard(boxX, boxY)
+                boxRect = pygame.Rect(left, top, Game.boxSize, Game.boxSize)
+                if boxRect.collidepoint(x, y):
+                    for card in self.__cards:
+                        if (left, top) == card.getCoords():
+                            return card
+        return None
+
+    def drawHighlightCard(self, coords):
+        """Draw the highlight"""
+        pygame.draw.rect(self.displaySurf, Game.highlightColor, (coords[0] - 5,
+            coords[1] - 5, Game.boxSize + 10, Game.boxSize + 10), 4)
+        pygame.display.flip()
+        pygame.time.wait(200)
+
+    def clicked(self, mX, mY):
+        """Box was clicked"""
+        card = self.getCardAtPixel(mX, mY)
+        if card:
+            self.drawHighlightCard(card.getCoords())
+            if card.covered != True:
+                card.covered = True
+                self.update()
+                pygame.display.flip()
+                if self.__firstCard == None:
+                    self.__firstCard = card
+                else:
+                    if (card.getShapeAndColor() !=
+                            self.__firstCard.getShapeAndColor()):
+                        pygame.time.wait(500)
+                        self.__firstCard.coverAnimation(self.displaySurf)
+                        card.coverAnimation(self.displaySurf)
+                        card.covered = False
+                        self.__firstCard.covered = False
+                    self.__firstCard = None
 
     def main(self):
         """docstring for main"""
         pygame.init()
         self.displaySurf.fill(Game.bgColor)
+        self.mouseX = 0
+        self.mouseY = 0
         while True:
-            self.displaySurf.fill(Game.bgColor)
+            #self.displaySurf.fill(Game.bgColor)
             self.update()
             for event in pygame.event.get():
                 if event.type == locals.QUIT or (event.type == locals.KEYDOWN
                         and event.key == locals.K_ESCAPE):
                     pygame.quit()
                     return
+                elif event.type == locals.MOUSEBUTTONUP:
+                    self.mouseX, self.mouseY = event.pos
+                    self.clicked(self.mouseX, self.mouseY)
 
             pygame.display.flip()
             Game.fpsClock.tick(30)
